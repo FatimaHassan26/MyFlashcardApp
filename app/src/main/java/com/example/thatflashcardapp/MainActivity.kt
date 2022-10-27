ac53bd441e46c9a333b78b1b9fed88ac14bb03bb
Sofiyah Afolabi <110312999+FatimaHassan26@users.noreply.github.com>
2022-10-27 00:33:31 -0500

    Update

--- app/src/main/java/com/example/thatflashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/thatflashcardapp/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.thatflashcardapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import android.view.ViewAnimationUtils
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -23,6 +28,8 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
         flashcardDatabase = FlashcardDatabase(this)
         allFlashcards = flashcardDatabase.getAllCards().toMutableList()
        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
@@ -36,8 +43,29 @@ class MainActivity : AppCompatActivity() {
 
 
         findViewById<TextView>(R.id.flashcard_question).setOnClickListener {
+            val answerSideView = findViewById<View>(R.id.flashcard_answer)
+
+
+
+// get the center for the clipping circle
+            val cx = answerSideView.width / 2
+            val cy = answerSideView.height / 2
+
+// get the final radius for the clipping circle
+
+// get the final radius for the clipping circle
+            val finalRadius = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+// create the animator for this view (the start radius is zero)
+
+// create the animator for this view (the start radius is zero)
+            val anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius)
+
             findViewById<TextView>(R.id.flashcard_answer).visibility = View.VISIBLE
             findViewById<TextView>(R.id.flashcard_question).visibility = View.INVISIBLE
+
+            anim.duration = 3000
+            anim.start()
         }
         Log.i("Fatima", "Question tab was clicked")
 
@@ -106,10 +134,32 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.addButton).setOnClickListener {
             val intent = Intent(this, AddCardActivity::class.java)
             resultLauncher.launch(intent)
+            overridePendingTransition(R.anim.leftin, R.anim.right_in)
 
         }
+        val leftOutAnim = AnimationUtils.loadAnimation(this, R.anim.leftin)
+        val rightInAnim = AnimationUtils.loadAnimation(this, R.anim.right_in)
+        leftOutAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // this method is called when the animation first starts
+                findViewById<View>(R.id.flashcard_question).startAnimation(leftOutAnim)
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // this method is called when the animation is finished playing
+                findViewById<View>(R.id.flashcard_question).startAnimation(rightInAnim)
+
+            }
+
+
+            override fun onAnimationRepeat(animation: Animation) {
+                // we don't need to worry about this method
+            }
+
+        })
         val nextButton = findViewById<ImageView>(R.id.flashcardnextbutton)
         nextButton.setOnClickListener{
+            findViewById<View>(R.id.flashcard_question).startAnimation(leftOutAnim)
             if (allFlashcards.isEmpty()) {
                 return@setOnClickListener
             }
@@ -126,7 +176,13 @@ class MainActivity : AppCompatActivity() {
             flashcardQuestion.text = question
             flashcardAnswer.text = answer
 
+
+
+
             }
+
+
+
         }
 }
 
